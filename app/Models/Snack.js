033f7d74cf78add6b225f1bd167ded050588e77d
Patrick <misner.patrick@gmail.com>
2022-05-24 14:26:22 -0600

    drawing

--- app/Models/Snack.js
+++ app/Models/Snack.js
@@ -10,4 +10,21 @@ export class Snack {
     this.qty = 0
   }
 
+  get Template(){
+    return `
+  <div class="col-4">
+    <div class="m-5 bg-dark text-light rounded shadow">
+      <img src="${this.img}" class="img-fluid rounded-top" alt="">
+      <div class="p-3">
+        <h3>${this.name}</h3>
+        <div class="d-flex justify-content-between">
+          <h3>${this.price.toFixed(2)}</h3>
+          <button id="${this.id}-button" onclick="buyItem(${this.id})" type="button" class="btn btn-primary">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+  }
+
 }
